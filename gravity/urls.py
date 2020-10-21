e553df13b5bed9017efa870e86707e10b1717edb
mitulbusa <mitulbusa1999@gmail.com>
2020-10-21 14:55:06 +0530

    anchor link css

--- gravity/urls.py
+++ gravity/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('apitosg', views.apitosg, name='apitosg'),
     path('sgtoapi', views.sgtoapi, name='sgtoapi'),
     path('contact', views.contact, name='contact'),
