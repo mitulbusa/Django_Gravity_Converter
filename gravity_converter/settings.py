8cb53c013cd6f028c4ecdf21bc8178d7b97cb74d
mitulbusa <mitulbusa1999@gmail.com>
2020-10-21 13:33:14 +0530

    ready to upload

--- gravity_converter/settings.py
+++ gravity_converter/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-9z$&cmx=9=9c7s_%llzjf@qmf7&r4^49l*28-rq_fxas6=4u7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gravity-converter.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
